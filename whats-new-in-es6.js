ab829c7a432ff6d331e70b0498c458f9f2444895
minhaz <minhaz.taher@gmail.com>
2020-03-04 13:52:50 +0600

    whats new in es6'

---
+++ whats-new-in-es6.js
@@ -0,0 +1,15 @@
+/*
+
+some  new cool feature of Es6 are:
+    Variable declaration with let and const
+    Blocks and IIFE
+    string literal 
+    Arrow function
+    Destructuring
+    Arrays
+    The spread Operator
+    Rest and default perimeter
+    Maps
+    Classes and Subclasses
+
+*/
