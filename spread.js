165044f5e94a212125199d9efccdf81b75eb0372
minhaz <minhaz.taher@gmail.com>
2020-03-04 21:58:17 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,16 @@
+//  interesting operation
+
+function add(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum = add(10, 18, 29, 40);
+
+console.log(sum);
+
+// es6;
+
+const sum6 = add(...ages);
+console.log(sum6);
+
+// and many more
